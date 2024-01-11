6bf1a1d3ec2c42af7ca4a67332544691ff0b29ac
@aymenmosrati6 <aymenmosrati6@gmail.com>
2024-01-11 10:19:45 +0100

    Add useLocalStorage hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { useDebounce } from "./hooks/useDebounce";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
   const [search, setSearch] = useState("");
   const [fetch, setFetch] = useState("");
+  const [value, setValue] = useState("");
   const debouncedSearch = useDebounce(search);
+  const { setItem, getItem, removeItem } = useLocalStorage("value");
 
   useEffect(() => {
     // fetch
@@ -15,6 +18,17 @@ function App() {
     <div className="App">
       <input type="text" onChange={(e) => setSearch(e.target.value)} />
       <>{fetch}</>
+      <div>
+        <h1>useLocalStorage</h1>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={() => setItem(value)}>Set</button>
+        <button onClick={() => console.log(getItem())}>Get</button>
+        <button onClick={removeItem}>Remove</button>
+      </div>
     </div>
   );
 }

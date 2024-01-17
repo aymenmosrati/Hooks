a0a2ee4c87d5ec8e854412e4d2b76dee6fe333ba
@aymenmosrati6 <aymenmosrati6@gmail.com>
2024-01-17 08:44:11 +0100

    Implement a generic request handler to improve code modularity and enhance reusability

---
+++ src/users.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { requestHandler } from "./hooks/requestHandler";
+interface User {
+  id: number;
+  name: string;
+}
+
+interface GetUsersParams {
+  limit?: number;
+  page?: number;
+}
+
+export const getUsers = requestHandler<GetUsersParams, User[]>((params) =>
+  axios.get("/api/users", { params })
+);

20eb3adf3de7c7c3e7ed9ae3e42c161c0c5d581d
@aymenmosrati6 <aymenmosrati6@gmail.com>
2024-02-13 11:19:49 +0100

    Added script for React component generation

---
+++ MyFirsyAuroComponent/MyFirsyAuroComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MyFirsyAuroComponent.css';
+
+const MyFirsyAuroComponent = () => {
+  return (
+    <div className="MyFirsyAuroComponent">
+      {/* Your component content here */}
+    </div>
+  );
+}
+
+export default MyFirsyAuroComponent;
